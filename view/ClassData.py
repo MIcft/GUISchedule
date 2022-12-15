075645b98942a4438af3be2c93cef5788c9c9fda
MIcft <crack135246@gmail.com>
2022-12-15 15:33:23 +0300

    Initial commit ddo

--- view/ClassData.py
+++ view/ClassData.py
@@ -2,44 +2,43 @@ import pprint
 
 
 class Data:
-    time_list = [
-        '8:00', '10:00',
-        '12:00', '14:00',
-        '16:00', '18:00'
-    ]
-
     def __init__(self, data, number_lesson, weekday):
         """
-        :param data: Входной json file
+        Инициализация класса Data который принимает данные из словаря и создает объект
+        с атрибутами, которые сохраняют в себе данные о занятии
+
+        :param data: Входной dict_file
         :param number_lesson: порядковый номер пары
         :param weekday: день недели начиная с понедельника
-        т.е 8:00 - 9:40 - 0 Пара
-        10:00 - 11:49 1 пара и т.д.
+
         """
         self.data = data
         self.number_lesson = number_lesson
-        self.weekday = weekday
+        self.weekday = weekday+1
         self.date = ''
         self.time_start = ''
         self.time_end = ''
         self.typeObj = ''
-        self.nameObj = ''
+        self.nameObj = ""
         self.nameTeach = ''
         self.auditories = ''
         self.get_all()
 
     def get_all(self):
         print('Connection')
-        # get  date in json
+        # get data in json
+
         for date in self.data['days']:
             if date['weekday'] == self.weekday:
-                self.date = date['date']
-                print(date['lessons'][self.number_lesson]['time_start'])
-                # if date['lessons']['time_start'] in Data.time_list[self.number_lesson]:
-                self.time_start = date['lessons'][self.number_lesson]['time_start']
-                self.time_end = date['lessons'][self.number_lesson]['time_end']
-                self.typeObj = date['lessons'][self.number_lesson]["typeObj"]["name"]
-                self.nameObj = date['lessons'][self.number_lesson]["subject"]
-                self.nameTeach = date['lessons'][self.number_lesson]["teachers"][0]["full_name"]
-                self.auditories = f'i{date["lessons"][self.number_lesson]["auditories"][0]["building"]["name"]} аудитория {date["lessons"][self.number_lesson]["auditories"][0]["name"]}'
-                print(self.nameObj)
+                try:
+                    self.date = date['date']
+                    self.time_start = date['lessons'][self.number_lesson]['time_start']
+                    self.time_end = date['lessons'][self.number_lesson]['time_end']
+                    self.typeObj = date['lessons'][self.number_lesson]["typeObj"]["name"]
+                    self.nameObj = date['lessons'][self.number_lesson]["subject"]
+                    self.nameTeach = date['lessons'][self.number_lesson]["teachers"][0]["full_name"]
+                    self.auditories = f'''
+i{date["lessons"][self.number_lesson]["auditories"][0]["building"]["name"]} аудитория 
+{date["lessons"][self.number_lesson]["auditories"][0]["name"]} '''
+                except TypeError: # if any datas in json is empty, assignment will be missing
+                    continue
